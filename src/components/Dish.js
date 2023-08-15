cb32c00bbd42a066c5b95465653fe8d586b74ea9
misterKumar <120304503+misterKumar@users.noreply.github.com>
2023-08-15 19:48:02 +0530

    check 6

--- src/components/Dish.js
+++ src/components/Dish.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Dish = ({ id, title, category, price, img, desc }) => {
   return (
-    <article className="dish" data-test-id={`menu-item-${category.toLowerCase()}`}>
+    <article className="dish" id={`menu-item-${category.toLowerCase()}`}>
       <p>Id :{id}</p>
       <img src={img} alt={title} />
       <div className="dish-info">
