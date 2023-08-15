3d52de544352fe58be8a8e747eb8316fb298d6e1
misterKumar <120304503+misterKumar@users.noreply.github.com>
2023-08-15 19:07:42 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,15 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import React from 'react';
+import DishApp from './DishApp';
+import '../styles/App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <DishApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Dish.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Dish = ({ id, title, category, price, img, desc }) => {
+  return (
+    <article className="dish">
+      <p>Id :{id}</p>
+      <img src={img} alt={title} />
+      <div className="dish-info">
+        <header>
+          <h4>{title}</h4>
+          <p className="price">${price}</p>
+        </header>
+        <p className="desc">{desc}</p>
+      </div>
+    </article>
+  );
+};
+
+export default Dish;
+
